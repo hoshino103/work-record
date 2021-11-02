3b652aeccb8f331d312c4801fb4017d0c56534b7
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-11-02 11:54:04 +0900

    records　テーブル作成

---
+++ db/migrate/20211102025233_create_records.rb
@@ -0,0 +1,12 @@
+class CreateRecords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :records do |t|
+      t.date :day_time
+      t.time :begin_time
+      t.time :finish_time
+      t.float :break_time
+      t.references :user, foreign_key: true, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_015326) do
+ActiveRecord::Schema.define(version: 2021_11_02_025233) do
+
+  create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "day_time"
+    t.time "begin_time"
+    t.time "finish_time"
+    t.float "break_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_11_02_015326) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "records", "users"
 end
